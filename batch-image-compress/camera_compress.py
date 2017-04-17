d8dcf98253492f27bf36a0050ee0676d63f5737a
zuoyuan <zuoyuan@meituan.com>
2017-04-17 22:20:07 +0800

    多线程改为多进程

--- batch-image-compress/camera_compress.py
+++ batch-image-compress/camera_compress.py
@@ -8,97 +8,77 @@ from os.path import join
 from PIL import Image
 import sys
 import threading, thread
+from multiprocessing import Pool
 import argparse
 
 
 def print_info(msg):
     print msg
-    
-file_list = []
-lock = thread.allocate_lock()
+
+#file_list = []
+#lock = thread.allocate_lock()
 
 def _compress_img(
-                max_size = 1080 # default 1080p
+                args
                 ):
-    
-    while True:
-        
-        
-        lock.acquire()
-        left = len(file_list)
-        
-        if left == 0:
-            lock.release()
-            return True
-            
-        src, target = file_list.pop(0)
-        
-        lock.release()
-        
-        
-        
-        if os.path.exists(target):
-            continue
-        
-        try:
-            im = Image.open(src)
-        except IOError:
-            continue
-            
-        width, height = im.size
-        
-        if width < height:
-            scale = 1.0 * max_size / width
-        else:
-            scale = 1.0 * max_size / height
-        scale = min(1.0, scale)
-        
-        new_size = int(width * scale), int(height * scale)
-        new_im = im.resize(new_size)
-        
-        if 'exif' in im.info:
-            new_im.save(target, 'JPEG', exif = im.info['exif'])
-        else:
-            new_im.save(target)
-            
-        print( 'Left %d. Current image is %s' % \
-                        (left, src)
-                )
-            
-def batch_compress_image(src, target, 
+    src, target, max_size = args  # default 1080p
+    if os.path.exists(target):
+        return
+
+    try:
+        im = Image.open(src)
+    except IOError:
+        return
+
+    width, height = im.size
+
+    if width < height:
+        scale = 1.0 * max_size / width
+    else:
+        scale = 1.0 * max_size / height
+    scale = min(1.0, scale)
+
+    new_size = int(width * scale), int(height * scale)
+    new_im = im.resize(new_size)
+
+    if 'exif' in im.info:
+        new_im.save(target, 'JPEG', exif = im.info['exif'])
+    else:
+        new_im.save(target)
+
+    print( 'Current image is %s' % \
+                    (src)
+            )
+
+def batch_compress_image(src, target,
                 max_size = 1080,    # default 1080p
                 extend = 'jpg,jpeg,png,bmp',  # default extend name
                 subdir = True,   # default include subdirectory
-                n_thread = 10     # default use 10 threads. 
+                n_thread = 10     # default use 10 threads.
                 ):
-                
+
     if not os.path.exists(src):
         print('Error: source directory not found!')
         return
-        
+
     extends = extend.split(',')
 
     fs = os.listdir(src)
     jpg_fs = [f for f in fs if f not in ('.', '..') and f[-3:].lower() in extends]
     sub_dir = [f for f in fs if f not in ('.', '..') and os.path.isdir(join(src, f))]
-    
+
     if not os.path.exists(target):
         os.mkdir(target)
-    
+
     n_img = len(jpg_fs)
     n_process = 0
+    file_list = []
     for f in jpg_fs:
-        file_list.append( (join(src, f), join(target, f)) )
+        file_list.append( (join(src, f), join(target, f), max_size) )
 
-    ts = []
-    for i in range(n_thread):
-        t = threading.Thread(target=_compress_img, args=(max_size,))
-        t.start()
-        ts.append(t)
-        
-    for t in ts:
-        t.join()
-        
+    pool = Pool(n_thread)
+    pool.map(_compress_img, file_list)
+
     if subdir:
         for d in sub_dir:
             batch_compress_image(join(src, d), join(target,d),
@@ -106,15 +86,15 @@ def batch_compress_image(src, target,
                         extend,
                         subdir)
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('src', help='source directory')
     parser.add_argument('target', help='target directory')
     parser.add_argument('--maxsize', '-s', type=int, default=1080, help='max pixel, eg: 720 1080')
     parser.add_argument('--nthread', '-n', type=int, default=10, help='How many threads.')
-    
-    
+
+
     args = parser.parse_args()
-    
+
     batch_compress_image(args.src, args.target, max_size=args.maxsize, n_thread=args.nthread)
     print 'Done!'
